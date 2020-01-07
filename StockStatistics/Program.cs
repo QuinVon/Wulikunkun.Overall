9e23ddf6ec8b5cdde692d358f791466daaff5338
wulikunkun <wulikunkun@outlook.com>
2020-01-07 10:28:42 +0800

    get请求碰到的一个问题

--- StockStatistics/Program.cs
+++ StockStatistics/Program.cs
@@ -11,29 +11,18 @@ namespace StockStatistics
     {
         static void Main(string[] args)
         {
-            string url = "http://api.waditu.com";
+            string url = "http://192.168.107.252:8056/monitor/cross/version";
             WebRequest request = WebRequest.Create(url);
-
-            #region 设置Request的请求体
-
-            string requestParams = "{\"api_name\": \"stock_basic\", \"token\": \"e1bcce57a2b55596f167e114d298e8ebc6e95d2f5385937fd00f09d9\", \"params\": {\"list_stauts\":\"L\"}, \"fields\": \"ts_code,name,area,industry,list_date\"}";
-            byte[] requestParamsBytes = Encoding.UTF8.GetBytes(requestParams);
-            request.Method = "post";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = requestParamsBytes.Length;
-            Stream requestParamsStream = request.GetRequestStream();
-            requestParamsStream.Write(requestParamsBytes, 0, requestParamsBytes.Length);
-            requestParamsStream.Close();
-
-            #endregion
-
-
+            //get请求不能设置请求体，但是可以设置请求头
+            request.Method = "get";
+            request.Headers.Add("token", "token");
             WebResponse response = request.GetResponse();
             Stream responseStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(responseStream, Encoding.Default);
             string responseFromServer = reader.ReadToEnd();
             string result = Decode.Unicode2String(responseFromServer);
-            Console.WriteLine(responseFromServer);
+            Console.WriteLine(result);
+            Console.Read();
         }
 
 
