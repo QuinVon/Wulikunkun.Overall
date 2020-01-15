aede89364f13d35c35adc71690ddba2aaf4db466
wulikunkun <wulikunkun@outlook.com>
2020-01-15 10:46:52 +0800

    修改工信部链接地址

--- StockStatistics/Program.cs
+++ StockStatistics/Program.cs
@@ -1,53 +1,15 @@
-﻿using NLog;
-using System;
-using Topshelf;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace StockStatistics
 {
-    public class StockStatistics
-    {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
-
-        public StockStatistics()
-        {
-
-        }
-
-        public void Start()
-        {
-            logger.Info("服务启动");
-        }
-
-        public void Stop()
-        {
-
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var rc = HostFactory.Run(x =>
-            {
-                x.Service<StockStatistics>(s =>
-                {
-                    s.ConstructUsing(name => new StockStatistics());
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
-                x.RunAsLocalSystem();
-
-                /* 配置服务随系统自动启动 */
-                x.StartAutomatically();
-                x.SetDescription("the stock statistics service");
-                x.SetDisplayName("StockStatistics");
-                x.SetServiceName("StockStatistics");
-            });
-
-            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
-            Environment.ExitCode = exitCode;
-
         }
     }
 }
--- StockStatistics/Properties/AssemblyInfo.cs
+++ StockStatistics/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("319a1eb3-c06e-4221-bc67-cd9731c9147e")]
+[assembly: Guid("fd8dda33-8af8-4090-8069-3f5ab1d19566")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
