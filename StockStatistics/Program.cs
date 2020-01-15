b6bfed0632bf048b359f80ddf4ac91d9fbdf5db9
wulikunkun <wulikunkun@outlook.com>
2020-01-15 09:58:26 +0800

    重新添加统计项目

--- StockStatistics/Program.cs
+++ StockStatistics/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.IO;
-using System.Text.RegularExpressions;
-using Utility;
+using System.Threading.Tasks;
 
 namespace StockStatistics
 {
@@ -11,20 +10,6 @@ namespace StockStatistics
     {
         static void Main(string[] args)
         {
-            string url = "http://192.168.107.252:8056/monitor/cross/version";
-            WebRequest request = WebRequest.Create(url);
-            //get请求不能设置请求体，但是可以设置请求头
-            request.Method = "get";
-            request.Headers.Add("token", "token");
-            WebResponse response = request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(responseStream, Encoding.Default);
-            string responseFromServer = reader.ReadToEnd();
-            string result = Decode.Unicode2String(responseFromServer);
-            Console.WriteLine(result);
-            Console.Read();
         }
-
-
     }
 }
