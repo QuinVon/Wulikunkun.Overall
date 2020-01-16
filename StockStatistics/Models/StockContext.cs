82bbf4bf3c1bf990cbc0e14155ab330919b09a86
wulikunkun <wulikunkun@outlook.com>
2020-01-16 11:18:22 +0800

    完成股价统计项目通过EF与MySQL进行交互

---
+++ StockStatistics/Models/StockContext.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.Entity;
+using System.Data.Entity;
+
+namespace StockStatistics.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class StockContext : DbContext
+    {
+        public StockContext() : base("name=TencentCloudMysql")
+        {
+
+        }
+    }
+}
