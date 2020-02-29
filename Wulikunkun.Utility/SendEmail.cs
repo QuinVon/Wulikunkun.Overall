5418da080e8326a5b45061476c43c7360050e0a7
wulikunkun <wulikunkun@outlook.com>
2020-02-29 17:02:38 +0800

    添加发送邮件功能

---
+++ Wulikunkun.Utility/SendEmail.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Wulikunkun.Utility
+{
+    public static class SendEmail
+    {
+        public static void Send(string address, string title, string content)
+        {
+            string sendAccount = "918407369@qq.com";
+            string authorizeCode = "fxemczwjopiibeij";
+            string receiver = address;
+            MailMessage message = new MailMessage();
+            MailAddress sendAddress = new MailAddress("918407369@qq.com");
+            message.From = sendAddress;
+            message.To.Add(receiver);
+            message.Subject = title;
+            message.Body = content;
+            SmtpClient client = new SmtpClient("smtp.qq.com", 25);
+            client.Credentials = new NetworkCredential(sendAccount, authorizeCode);
+            client.EnableSsl = true;
+            client.Send(message);
+        }
+    }
+}
