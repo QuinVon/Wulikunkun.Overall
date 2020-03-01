52f924c650ddaba487376806aae3e722b2756050
wulikunkun <wulikunkun@outlook.com>
2020-03-01 09:58:25 +0800

    添加一个测试项目

---
+++ Wulikunkun.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Wulikunkun.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        
+    }
+
+    public class Container
+    {
+        
+    }
+
+    public class Registry
+    {
+        
+    }
+}
