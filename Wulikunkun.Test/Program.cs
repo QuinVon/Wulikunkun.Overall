17886429f62959bbea6c10eefd57f2be50dd7512
wulikunkun <wulikunkun@outlook.com>
2020-03-01 14:32:18 +0800

    临时签入没有设计完成的依赖注入框架

--- Wulikunkun.Test/Program.cs
+++ Wulikunkun.Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Reflection;
 
 namespace Wulikunkun.Test
 {
@@ -6,19 +8,67 @@ namespace Wulikunkun.Test
     {
         static void Main(string[] args)
         {
+            Type type = typeof(IFoo<,>);
+            Type personType = typeof(Person);
+            Person person = new Person("wangkun", "24");
+            ConstructorInfo[] constructors = personType.GetConstructors(BindingFlags.Instance);
             Console.WriteLine("Hello World!");
         }
-        
-        
+    }
+
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public class InjectionAttribute : Attribute
+    {
+    }
+
+    public enum LifeTime
+    {
+        Transient,
+        Scope,
+        Root
+    }
+
+    public class Foo : IFoo<Container, Registry>
+    {
+    }
+
+    public interface IFoo<T1, T2>
+    {
+    }
+
+    public interface IBar<T>
+    {
+    }
+
+    public class PersonBase
+    {
+        public string School { get; set; }
+        public string Directory { get; set; }
+
+        public PersonBase(string school, string directory)
+        {
+            this.School = school;
+            this.Directory = directory;
+        }
+    }
+
+    public class Person : PersonBase, IBar<Container>
+    {
+        public string Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name, string age) : base(name, age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
     }
 
     public class Container
     {
-        
     }
 
     public class Registry
     {
-        
     }
 }
