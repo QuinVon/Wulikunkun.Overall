fc92e448d3c59f9c92cd242cf22ddbb5a1a507b5
王昆 <5627379+wulikunkun@user.noreply.gitee.com>
2020-07-21 14:17:55 +0800

    调试成功CKEditor4自定义插件

--- Wulikunkun.Web/wwwroot/js/ckeditor/plugins/submitbutton/plugin.js
+++ Wulikunkun.Web/wwwroot/js/ckeditor/plugins/submitbutton/plugin.js
@@ -1,17 +1,18 @@
 ﻿CKEDITOR.plugins.add('submitbutton', {
     icons: 'submitbutton',
     init: function (editor) {
+        debugger;
         editor.addCommand('insertTimestamp', {
             exec: function (editor) {
                 var now = new Date();
                 editor.insertHtml('The current date and time is: <em>' + now.toString() + '</em>');
             }
         });
-        editor.ui.addButton('SubmitButton', {
+        editor.ui.addButton('submitbutton', {
             label: 'Insert Timestamp',
             command: 'insertTimestamp',
             toolbar: 'editing',
-            icon: this.path + '/icons/submitbutton.png'  // icon file (PNG)
+            icon: this.path + 'icons/submitbutton.png'  // icon file (PNG)
         });
     }
 });
