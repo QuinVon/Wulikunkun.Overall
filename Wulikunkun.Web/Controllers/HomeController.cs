102bfe6ecdfea1ce937e6f7f1019b0a10f8454e9
wulikunkun <wulikunkun@outlook.com>
2020-10-18 11:00:31 -0400

    初步完成首页登录成功后的跳转功能

--- Wulikunkun.Web/Controllers/HomeController.cs
+++ Wulikunkun.Web/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace Wulikunkun.Web.Controllers
         {
             if (HttpContext.Session.IsAvailable)
             {
-                string username = HttpContext.Session.GetString("username");
+                string username = HttpContext.Session.GetString("UserName");
                 if (username != null)
                 {
                     ViewBag.UserName = username;
@@ -43,6 +43,7 @@ namespace Wulikunkun.Web.Controllers
             var passwordAndSaltBytes = Encoding.UTF8.GetBytes(user.Password + userSalt);
             var hashBytes = new SHA256Managed().ComputeHash(passwordAndSaltBytes);
             var hashString = Convert.ToBase64String(hashBytes);
+            HttpContext.Session.SetString("UserName",user.Name);
             if (hashString == corrUser.Password)
             {
                 return Json(new
@@ -56,93 +57,5 @@ namespace Wulikunkun.Web.Controllers
                 StatusCode = 0
             });
         }
-
-        public IActionResult Admin()
-        {
-            return View();
-        }
-
-        public IActionResult AdminIndex()
-        {
-            return View();
-        }
-
-        public ViewResult PageDetail()
-        {
-            return View();
-        }
-
-        public ViewResult Editor()
-        {
-            return View();
-        }
-
-        public void Edit(string markdownDoc, string htmlCode)
-        {
-        }
-
-        #region 框架自带代码
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
-
-        #endregion
-
-        #region Swagger测试部分
-
-        [HttpGet("{id}")]
-        public ActionResult<string> GetUsers(int id)
-        {
-            return $"Get Num {id} Info";
-        }
-
-        [HttpPost("{id}")]
-        public ActionResult<string> Edit(int id)
-        {
-            return $"Modify Num {id} info";
-        }
-
-        [HttpPut("{id}")]
-        public ActionResult<string> Add(int id)
-        {
-            return $"Add Num {id} info";
-        }
-
-        ///summary
-        ///to get user info
-        ///summary
-        [HttpPost]
-        [Route("UserInfo")]
-        public JsonResult UserInfo(string uid, string password, string time)
-        {
-            object responseData = new
-            {
-                responseDescription = "响应成功",
-                responseState = "0",
-                UserInfo = new
-                {
-                    userId = "stu001",
-                    firstName = "刘晓阳",
-                    lastName = "数学学院"
-                }
-            };
-            return Json(responseData);
-        }
-
-        [HttpDelete("{id}")]
-        public ActionResult<string> Delete(int id)
-        {
-            return $"Delete Num {id} info";
-        }
-
-        #endregion
     }
 }
---
+++ Wulikunkun.Web/Migrations/20201018073815_添加文章审核字段.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Migrations
+{
+    public partial class 添加文章审核字段 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAllowed",
+                table: "Articles",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAllowed",
+                table: "Articles");
+        }
+    }
+}
